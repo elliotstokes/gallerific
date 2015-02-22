746df7296a4d62c92013af088b09077326575e92
Elliot Stokes <elliot.stokes@gmail.com>
2015-02-22 19:08:07 +0000

    Major perf improvmenents. Add in film grain.

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -4,7 +4,14 @@
     paths: {
       'THREE': 'lib/threejs/build/three.min',
       'FirstPersonControls': 'lib/FirstPersonControls/THREE.FirstPersonControls',
-      'stats': 'https://cdnjs.cloudflare.com/ajax/libs/stats.js/r11/Stats.min',
+      'FilmGrain' : 'lib/FilmGrain/FilmShader',
+      'FilmPass' : 'lib/FilmPass/FilmPass',
+      'EffectComposer' : 'lib/EffectComposer/EffectComposer',
+      'CopyShader': 'lib/CopyShader/CopyShader',
+      'MaskPass': 'lib/MaskPass/MaskPass',
+      'RenderPass': 'lib/RenderPass/RenderPass',
+      'ShaderPass': 'lib/ShaderPass/ShaderPass',
+      'stats': 'lib/stats/Stats.min',
       'text' : 'lib/text/text',
       'json': 'lib/requirejs-plugins/src/json'
     },
@@ -16,7 +23,35 @@
       FirstPersonControls: { 
       	deps: ['THREE'], 
       	exports: 'THREE' 
-      }
+      },
+      CopyShader : {
+        deps: ['THREE'], 
+        exports: 'THREE' 
+      },
+      ShaderPass : {
+        deps: ['THREE'], 
+        exports: 'THREE' 
+      },
+      RenderPass : {
+        deps: ['THREE'], 
+        exports: 'THREE' 
+      },
+      MaskPass : {
+        deps: ['THREE'], 
+        exports: 'THREE' 
+      },
+      EffectComposer: { 
+        deps: ['THREE', 'CopyShader', 'ShaderPass','MaskPass'], 
+        exports: 'THREE' 
+      },
+      FilmGrain: { 
+        deps: ['THREE'], 
+        exports: 'THREE' 
+      },
+      FilmPass: { 
+        deps: ['THREE','FilmGrain'], 
+        exports: 'THREE' 
+      },
     }
   });
 
@@ -27,8 +62,13 @@
     'instagram',
     'THREE',
     'stats',
-    'FirstPersonControls'
-  ], function(settings, Map, Collision, Instagram, THREE, Stats) {
+    'generator',
+    'FirstPersonControls',
+    'EffectComposer',
+    'FilmPass',
+    'RenderPass',
+    'CopyShader'
+  ], function(settings, Map, Collision, Instagram, THREE, Stats, generator) {
 
     var stats = new Stats();
     stats.setMode(0); // 0: fps, 1: ms
@@ -43,7 +83,8 @@
     var scene = new THREE.Scene();
     var collider;
     var instagram = new Instagram('7e86b13292f34aa0b0115cbe18aba06b');
-    var map = new Map();
+    var mapLayout = generator.generate();
+    var map = new Map(mapLayout);
 
 
     instagram.getPopularImages(function(media) {
@@ -51,7 +92,7 @@
     	// create a WebGL renderer, camera
     	// and a scene
     	var renderer = new THREE.WebGLRenderer();
-      var camera = new THREE.PerspectiveCamera(60, settings.ASPECT, 1, 3000); // Field Of Viw, aspect ratio, near, far
+      var camera = new THREE.PerspectiveCamera(60, settings.ASPECT, 1, 3500); // Field Of Viw, aspect ratio, near, far
 
       camera.position.y = settings.UNITSIZE * 0.4; // Raise the camera off the ground
       //set the camera to be in the top left
@@ -68,9 +109,17 @@
 
       // start the renderer
       renderer.setClearColor( 0xbfd1e5 );
-      //renderer.setPixelRatio( window.devicePixelRatio );
       renderer.setSize(settings.WIDTH, settings.HEIGHT);
-
+      var rtParameters = { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter, format: THREE.RGBFormat, stencilBuffer: true };
+      var effectComposer = new THREE.EffectComposer( renderer, new THREE.WebGLRenderTarget( settings.WIDTH, settings.HEIGHT, rtParameters ) );
+      effectComposer.setSize( settings.WIDTH, settings.HEIGHT );
+      var effectFilm = new THREE.FilmPass( 0.35, 0.025, 648, false );
+      renderPass = new THREE.RenderPass( scene, camera );
+      effectComposer.addPass(renderPass);
+      effectComposer.addPass(effectFilm);
+      var copyPass = new THREE.ShaderPass( THREE.CopyShader);
+      effectComposer.addPass(copyPass);
+      copyPass.renderToScreen = true;
       // attach the render-supplied DOM element
       document.getElementById('container').appendChild(renderer.domElement);
 
@@ -86,11 +135,11 @@
         requestAnimationFrame( animate );
       }
 
-
       // ## Render the 3D Scene
       function render() {
         // actually display the scene in the Dom element
         renderer.render( scene, camera);
+        effectComposer.render(0.01);
       }
 
       map.create(scene, media);
--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -5,13 +5,8 @@
  define([
   'THREE',
   'settings',
-  'generator'
-  ], function(THREE, settings, generator) {
+  ], function(THREE, settings) {
 
-
-    var mapLayout = generator.generate(),
-        mapW = mapLayout.length, 
-        mapH = mapLayout[0].length;
 
     var textures = [
       THREE.ImageUtils.loadTexture( 'images/textures/floor.jpg' ),
@@ -25,60 +20,35 @@
      * @class
      * @classdesc Generates and manages the map
      **/
-    var map = function() {
-      var units = Math.max(mapW, mapH),
+    var map = function(mapLayout) {
+      var mapW = mapLayout.length, 
+          mapH = mapLayout[0].length,
+          units = Math.max(mapW, mapH),
           obstacles = [];
+
       textures[0].wrapS = textures[1].wrapS =  textures[2].wrapS =  THREE.RepeatWrapping;
       textures[0].wrapT = textures[1].wrapT =  textures[2].wrapT =  THREE.RepeatWrapping;
-      textures[0].repeat.set(2*units,2*units);
-      textures[2].repeat.set(2*units,2*units);
+      textures[0].repeat.set(2,2);
+      textures[2].repeat.set(2,2);
       textures[1].repeat.set(1,2);
 
-
-      var floor = new THREE.Mesh(
-        new THREE.BoxGeometry(units * settings.UNITSIZE, 10, units * settings.UNITSIZE),
-        new THREE.MeshPhongMaterial({map:textures[0], shading: THREE.NoShading})
-      );
-
-
-      var ceiling = new THREE.Mesh(
-        new THREE.BoxGeometry(units * settings.UNITSIZE, 10, units * settings.UNITSIZE),
-         new THREE.MeshPhongMaterial({map:textures[2],shading: THREE.NoShading})
-      );
-
-      ceiling.position.y = settings.WALLHEIGHT;
-
       var cube = new THREE.BoxGeometry(settings.UNITSIZE, settings.WALLHEIGHT, settings.UNITSIZE);
+      var tile = new THREE.BoxGeometry(settings.UNITSIZE, 10, settings.UNITSIZE);
       var materials = [
-        new THREE.MeshLambertMaterial({map:textures[1]})
+        new THREE.MeshLambertMaterial({map:textures[1]}),
+        new THREE.MeshLambertMaterial({map:textures[0]}),
+        new THREE.MeshPhongMaterial({map:textures[2]})
+
       ];
 
       var pictureGeometry = new THREE.BoxGeometry(100, 100, 5);
       var pictureGeometryZ = new THREE.BoxGeometry(5, 100, 100);
-      
-      function addChair(scene) {
-        var loader = new THREE.JSONLoader(); // init the loader util
-
-        // init loading
-        loader.load('models/chair.json', function (geometry) {
-        // create a new material
-  
-          // create a mesh with models geometry and material
-          var mesh = new THREE.Mesh(
-            geometry,
-            new THREE.MeshLambertMaterial({map:textures[1]})
-          );
-
-          scene.add(mesh);
-        });
-      }
 
       /**
        * Consructs the map
        **/
       this.create = function(scene, photos) {
 
-        //addChair(scene);
 
         THREE.ImageUtils.crossOrigin = '';
         var galleryTextures = [];
@@ -88,7 +58,8 @@
         });
 
         var material = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-        var totalGeom = new THREE.Geometry();
+        var walllGeom = new THREE.Geometry();
+        var floorGeom = new THREE.Geometry();
         var lightsGeom = new THREE.Geometry();
         var imageIndex = 0;
         for (var i = 0; i < mapW; i++) {
@@ -99,12 +70,17 @@
               wall.position.y = settings.WALLHEIGHT/2;
               wall.position.z = (j - units/2) * settings.UNITSIZE;
               wall.updateMatrix();
-              totalGeom.merge( wall.geometry, wall.matrix );
+              walllGeom.merge( wall.geometry, wall.matrix );
             } else {
+              var floorTile = new THREE.Mesh(tile, null);
+              floorTile.position.x = (i - units/2) * settings.UNITSIZE;
+              floorTile.position.z = (j - units/2) * settings.UNITSIZE;
+              floorTile.updateMatrix();
+              floorGeom.merge( floorTile.geometry, floorTile.matrix );
 
               var x = (i - units/2) * settings.UNITSIZE;
               var z = (j - units/2) * settings.UNITSIZE;
-              if ((j+i)%5 ===0) {
+              if ((j+i)%4 ===0) {
                 var geometry = new THREE.SphereGeometry( 5, 32, 32 );
                 var sphere = new THREE.Mesh( geometry, null );
 
@@ -152,16 +128,24 @@
             }
           }
         }
-        //totalGeom.geometry.computeBoundingSphere();
-        var walls = new THREE.Mesh(totalGeom,materials[0]);
+
+
+        var walls = new THREE.Mesh(walllGeom,materials[0]);
         scene.add(walls);
         obstacles.push(walls);
         var spheres = new THREE.Mesh(lightsGeom, material);
         scene.add(spheres);
-        scene.fog = new THREE.FogExp2(0xD6F1FF, 0.0004);
-        obstacles.push(floor);
-        scene.add(floor);
+
+        var floor = new THREE.Mesh(floorGeom,materials[1]);
+        var ceilingGeom = floorGeom.clone();
+        
+
+        var ceiling = new THREE.Mesh(ceilingGeom,materials[2]);
+        ceiling.position.y =  settings.WALLHEIGHT;
         scene.add(ceiling);
+        scene.add(floor);
+
+        scene.fog = new THREE.FogExp2(0xD6F1FF, 0.0004);
 
         
         var ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white light
